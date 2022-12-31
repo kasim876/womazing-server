ec46e49520323ceac3366e06f2b3978d613c974c
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-31 12:54:17 +0300

    add db connect

---
+++ db.js
@@ -0,0 +1,39 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+});
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  oldPrice: {
+    type: DataTypes.INTEGER,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  img: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+})
+
+module.exports = {
+  sequelize,
+  Product,
+};
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 
 const app = express();
 
 app.use(express.json());
 
-const start = () => {
-  app.listen(process.env.PORT, () => console.log('Порт запущен на сервере %d', process.env.PORT));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(process.env.PORT, () => console.log('Порт запущен на сервере %d', process.env.PORT));
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 start();
