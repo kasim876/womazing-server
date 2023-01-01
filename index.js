6a5c48e5e39b32f23d017e07ec452ecb01ccb765
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-02 02:53:36 +0300

    add static

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
+const cors = require('cors');
+const { sequelize } = require('./db');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static('static'));
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(process.env.PORT, () => console.log('Порт запущен на сервере %d', process.env.PORT));
+    app.listen(process.env.PORT, () => console.log('Server started on port %d', process.env.PORT));
   } catch (error) {
     console.log(error)
   }
