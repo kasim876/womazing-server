a0514ba6b2c4179d163635b31d27545ae77b9cd7
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-01 21:57:41 +0200

    edit db connection

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const mysql2 = require('mysql2'); // Needed to fix sequelize issues with WebPack
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    host: process.env.DB_HOST,
+  }
+);
 
 const Product = sequelize.define('Product', {
   id: {
